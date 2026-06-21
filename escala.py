3656fb05c1b3b16fd21943d95664b42f3d433081
sergio231973 <37090578+sergio231973@users.noreply.github.com>
2026-06-21 04:24:00 -0300

    Update escala.py

--- escala.py
+++ escala.py
@@ -18,7 +18,7 @@ def carregar():
 
          # Garantir estrutura da Viradinha Ouro
         dados.setdefault("viradinha_fila", [
-            "Sergio", "Cardoso", "Daniel", "Caio"
+            "Sergio", "Cardoso", "Daniel", "Caio",
             "Washington", "Digones", "Anderson", "Wilian",
         ])
         dados.setdefault("viradinha_historico", [])
@@ -32,7 +32,7 @@ def carregar():
         # Garantir chaves novas
         dados.setdefault("viradinha_fila", [
             "Washington", "Digones", "Anderson", "Wilian",
-            "Sergio", "Cardoso", "Daniel", "Caio"
+            "Sergio", "Cardoso", "Daniel", "Caio",
         ])
         dados.setdefault("viradinha_historico", [])
         return dados
